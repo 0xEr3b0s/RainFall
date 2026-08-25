dcbd955112b69b7605c24738791d9ad621a4ecbe
H3XploR <73852348+H3XploR@users.noreply.github.com>
2026-08-25 20:19:46 +0200

    updated: source added

---
+++ level02/script.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+
+static void p(void)
+{
+  uint unaff_retaddr;
+  char local_50 [76];
+  
+  fflush(stdout);
+  gets(local_50);
+  if ((unaff_retaddr & 0xb0000000) == 0xb0000000) {
+    printf("(%p)\n", &unaff_retaddr);
+                    /* WARNING: Subroutine does not return */
+    exit(1);
+  }
+  puts(local_50);
+  strdup(local_50);
+  return;
+}
+
+
+int main(void)
+{
+  p();
+  return 0;
+}
